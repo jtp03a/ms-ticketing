bbd6cad78f3c1557f8e74cddc037fafdb88e1023
Jake Peterson <a3sir86@gmail.com>
2021-05-19 09:19:36 -0600

    adds deployment step and test endpoint

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,10 @@ const app = express()
 
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('hit current user endpoint')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
