7bdd5e27922f8bbe0f37093c6a1c835262850fda
Jake Peterson <a3sir86@gmail.com>
2021-05-19 13:02:54 -0600

    136

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,8 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
-
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response , next: NextFunction) => {
-  console.log('Something went wrong', err)
+  if (err instanceof RequestValidationError) {
+    console.log('request val error')
+  }
+  if (err instanceof DatabaseConnectionError) {
+    console.log('db conn error')
+  }
   res.status(400).send({
     message: err.message
   })
