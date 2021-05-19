be20cd64164f201fe4fb0a805002d611798c3709
Jake Peterson <a3sir86@gmail.com>
2021-05-19 13:13:27 -0600

    137

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,10 +4,13 @@ import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response , next: NextFunction) => {
   if (err instanceof RequestValidationError) {
-    console.log('request val error')
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param }
+    })
+    return res.status(400).send({ errors: formattedErrors})
   }
   if (err instanceof DatabaseConnectionError) {
-    console.log('db conn error')
+    return res.status(500).send({ errors: [{ message: err.reason }]})
   }
   res.status(400).send({
     message: err.message
