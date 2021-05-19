b64d62b33ca8bae90ff44af1cea99a44b08de0ac
Jake Peterson <a3sir86@gmail.com>
2021-05-19 13:48:17 -0600

    140

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,11 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from './custom-error'
+
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500
   reason = 'Error connecting to database'
   
   constructor() {
-    super()
+    super('Error connecting to database')
 
     //Only because extending build in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
-import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response , next: NextFunction) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors()})
   }
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors()})
-  }
+  
   res.status(400).send({
     errors: [{message: 'Something went wrong'}]
   })
