d7aaaf025e3a5c221ea42152b8527f74985424b7
Jake Peterson <a3sir86@gmamil.com>
2021-05-21 10:04:44 -0600

    177

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -31,7 +31,10 @@ const userSchema = new mongoose.Schema({
   {
     toJSON: {
       transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
         delete ret.password
+        delete ret.__v
       }
     }
   }
