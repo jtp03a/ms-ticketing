49bc9ba354c8b1d15b3953369f2808ad5871792e
Jake Peterson <a3sir86@gmamil.com>
2021-05-21 11:24:32 -0600

    187

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import { currentUser } from '../middlewares/current-user'
+import { requireAuth } from '../middlewares/require-auth'
 
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
   res.send({currentUser: req.currentUser || null })
 })
 
