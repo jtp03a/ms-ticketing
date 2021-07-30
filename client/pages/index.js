a81de9c3984959e8ed9af1bb70f61aaaf4bf746f
Jake Peterson <a3sir86@gmail.com>
2021-07-30 13:13:33 -0500

    221

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,13 @@
-const Index = () => {
-  return (<h1>Landing Page</h1>)
+import axios from 'axios'
+
+const LandingPage = ({ color }) => {
+  console.log('I am in the component', color)
+  return <h1>LandingPage</h1>
 }
 
 LandingPage.getInitialProps = () => {
-  
+  console.log('I am on the server')
+  return { color: 'red'}
 }
-
-export default Index
+ 
+export default LandingPage
