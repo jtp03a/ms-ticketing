a97d4005e1300c81295171e47b9a5d57a7f23860
Jake Peterson <a3sir86@gmamil.com>
2021-08-25 14:47:45 -0600

    backup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
+import axios from 'axios'
+
+const LandingPage = ({ color }) => {
+  console.log('I am in the component', color)
+  return <h1>LandingPage</h1>
+}
+
+LandingPage.getInitialProps = ()=> {
+  console.log
+}
+ 
 const Index = () => {
   return (<h1>Landing Page</h1>)
 }
