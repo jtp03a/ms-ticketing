002fd5b9733e9a22c6f990ddd3e0f057d5d4d532
Jake Peterson <a3sir86@gmail.com>
2022-02-01 18:38:30 -0600

    try external url

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,10 +5,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>LandingPage</h1>
 }
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers)
   if (typeof window === 'undefined') {
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: { Host: 'ms-ticketing.jakepeterson.dev'}
+    const { data } = await axios.get('https://ms-ticketing.jakepeterson.dev/api/users/currentuser', {
+      headers: req.headers
     })
     return data
   } else {
