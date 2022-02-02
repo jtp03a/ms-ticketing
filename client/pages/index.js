8f26f39e08031d4b7394a48f5ae00744cee796ef
Jake Peterson <a3sir86@gmail.com>
2022-02-01 20:59:25 -0600

    changes axios request to cluster internal

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,7 +8,7 @@ const LandingPage = ({ currentUser }) => {
 LandingPage.getInitialProps = async ({ req }) => {
   console.log(req.headers)
   if (typeof window === 'undefined') {
-    const { data } = await axios.get('https://ms-ticketing.jakepeterson.dev/api/users/currentuser', {
+    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
       headers: req.headers
     })
     return data
