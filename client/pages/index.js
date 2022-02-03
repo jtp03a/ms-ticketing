d32493c9d5e248a174ec0ba8656782b26cb5ee16
Jake Peterson <a3sir86@gmail.com>
2022-02-03 10:22:31 -0600

    235

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import BuildClient from "../api/build-client"
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser)
@@ -6,16 +6,8 @@ const LandingPage = ({ currentUser }) => {
 }
 
 LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers)
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-      headers: req.headers
-    })
-    return data
-  } else {
-    const { data } = await axios.get('/api/users/currentuser')
-    return data
-  }
+  const { data } = await BuildClient(context).get('/api/users/currentuser')
+  return data
 }
  
 export default LandingPage
