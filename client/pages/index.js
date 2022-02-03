f0be3baf5037364d7dc3af03f613987e75f23a65
Jake Peterson <a3sir86@gmail.com>
2022-02-03 10:23:30 -0600

    235

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,7 +5,7 @@ const LandingPage = ({ currentUser }) => {
   return <h1>LandingPage</h1>
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
+LandingPage.getInitialProps = async (context) => {
   const { data } = await BuildClient(context).get('/api/users/currentuser')
   return data
 }
