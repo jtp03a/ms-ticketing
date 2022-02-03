f1ccc01d526bc26ec7c1f8b15c6f65c12086d7d1
Jake Peterson <a3sir86@gmail.com>
2022-02-03 10:58:22 -0600

    237

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-const SignUp = () => {
+const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { doRequest, errors } = useRequest({
-    url: '/api/users/signup',
+    url: '/api/users/signin',
     method: 'post',
     body: {
       email,
@@ -23,7 +23,7 @@ const SignUp = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
+      <h1>Sign In</h1>
       <div className="form-group">
         <label>Email Address</label>
         <input
@@ -47,4 +47,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp
+export default SignIn
