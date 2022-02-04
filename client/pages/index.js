f1eaaa6da31caecb5d97132b86e2c54f6bb28ca9
Jake Peterson <a3sir86@gmail.com>
2022-02-04 14:28:12 -0600

    test only options in pipeline

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import BuildClient from "../api/build-client"
 
 const LandingPage = ({ currentUser }) => {
-  return currentUser ? ( <h1>You are signed in! </h1> ) : ( <h1>You are NOT signed in!</h1> )
+  return currentUser ? ( <h1>You are signed in </h1> ) : ( <h1>You are NOT signed in</h1> )
 }
 
 LandingPage.getInitialProps = async (context) => {
