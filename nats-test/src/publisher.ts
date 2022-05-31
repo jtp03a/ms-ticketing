e893e0bab680cb1a67aef1e6555b74ff3fa6e758
Jake Peterson <a3sir86@gmail.com>
2022-05-31 13:26:26 -0500

    test publish data

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,5 +7,16 @@ const client = nats.connect('ticketing', 'abc', {
 
 client.on('connect', () => {
   console.log('Publisher connected to NATS')
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concernt',
+    price: 20
+  })
+
+  client.publish('ticket:created', data, () => {
+    console.log('Event Published')
+  })
+  
 })
 
