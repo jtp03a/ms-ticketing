3616053d3e84311829d07273f8b7ac37a0933b52
Jake Peterson <a3sir86@gmail.com>
2022-06-10 11:04:11 -0500

    fix git ignores

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,39 @@
-import nats from 'node-nats-streaming'
+import nats, { Message }  from 'node-nats-streaming'
+import { randomBytes } from 'crypto';
 
-const client = nats.connect('ticketing', '123', {
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://nats-srv:4222'
 })
 
 client.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = client.subscribe('ticket:created')
-
-  subscription.on('message', (msg) => {
-    console.log('Message recieved');
+  client.on('close', () => {
+    console.log('NATS connection closed')
+    process.exit()
   })
+
+  const options = client.subscriptionOptions()
+    .setManualAckMode(true)
+
+  const subscription = client.subscribe('ticket:created', 'orders-service-queue-group', options)
+
+  subscription.on('message', (msg: Message) => {
+    console.log(msg.getSequence());
+    const data = msg.getData();
+
+    if(typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
+    }
+
+    msg.ack()
+  })
+})
+
+process.on('SIGINT', () => {
+  client.close()
+})
+
+process.on('SIGTERM', () => {
+  client.close()
 })
